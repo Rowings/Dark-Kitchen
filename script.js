c1f95e5f7a69a9289f349b4ad10fb07a4b64d75e
Rowings <zakariaazarkan@gmail.com>
2022-04-26 15:31:58 +0200

    version temporaire, changement d'idée de procédé

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 const collection = [
     {
       type: "Tacos",
@@ -8,44 +15,38 @@ const collection = [
     },
 
     {
-        type: "Burger",
-        genre: ["Simple", "Double", "Triple", "DIVIN"]
-        viande: ["Angus", "Steak", "Veggie", "Hamburger"]
-        sauce: "Andalouse, Brazil, Samouraï, Barbecue, Blanche, Curry-Ketchup, Biggie, Chili-mayo, Ketchup, Mayonnaise, Americain ",
-        price: [15, 25, 35, 45]
-      },
-    // {
-    //     type: "Tacos",
-    //     genre: "Double",
-    //     viande: "Nuggets, Tenders, Cordon Bleu, Steak, Veggie, Falafel",
-    //     sauce: "Andalouse, Brazil, Samouraï, Barbecue, Blanche, Curry-Ketchup, Biggie, Chili-mayo, Ketchup, Mayonnaise, Americain ",
-    //     price: 35,
-    //   },
-    //   {
-    //     type: "Tacos",
-    //     genre: " Triple",
-    //     viande: "Nuggets, Tenders, Cordon Bleu, Steak, Veggie, Falafel",
-    //     sauce: "Andalouse, Brazil, Samouraï, Barbecue, Blanche, Curry-Ketchup, Biggie, Chili-mayo, Ketchup, Mayonnaise, Americain ",
-    //     price : 45,
-    //   },
-    //   {
-    //     type: "Tacos",
-    //     genre: "Gratinée",
-    //     viande: "Nuggets, Tenders, Cordon Bleu, Steak, Veggie, Falafel",
-    //     sauce: "Andalouse, Brazil, Samouraï, Barbecue, Blanche, Curry-Ketchup, Biggie, Chili-mayo, Ketchup, Mayonnaise, Americain ",
-    //     price : 65,
-    //   },
+    type: "Tacos",
+    genre: "Double",
+    viande: "Nuggets, Tenders, Cordon Bleu, Steak, Veggie, Falafel",
+    sauce: "Andalouse, Brazil, Samouraï, Barbecue, Blanche, Curry-Ketchup, Biggie, Chili-mayo, Ketchup, Mayonnaise, Americain ",
+    price: 35,
+    },
+    {
+    type: "Tacos",
+    genre: " Triple",
+    viande: "Nuggets, Tenders, Cordon Bleu, Steak, Veggie, Falafel",
+    sauce: "Andalouse, Brazil, Samouraï, Barbecue, Blanche, Curry-Ketchup, Biggie, Chili-mayo, Ketchup, Mayonnaise, Americain ",
+    price : 45,
+    },
+    {
+    type: "Tacos",
+    genre: "Gratinée",
+    viande: "Nuggets, Tenders, Cordon Bleu, Steak, Veggie, Falafel",
+    sauce: "Andalouse, Brazil, Samouraï, Barbecue, Blanche, Curry-Ketchup, Biggie, Chili-mayo, Ketchup, Mayonnaise, Americain ",
+    price : 65,
+    },
   
   
   
 
 
     {
-        name: "Medine",
-        labels: " Din Records, Because Music, WMI, Stars de rue",
-        dateNaissance: "Date de Naissance : 1983",
-        picture: "https://upload.wikimedia.org/wikipedia/commons/c/c7/GRANDm%C3%A9dine.jpg",
-        genre: ["Hip-hop", "Rap Français", "Rap politique", "Rap Hardcore", "Rap conscient"],
-        album : ["11 Septembre, récit du 11e jour", "Jihad, le plus grand combat contre soi-même", "Arabian Panther", "Protest Song", "Storyteller", "Grand Medine"],
-        link : "https://www.youtube.com/watch?v=tHUp9Bb9_Sk"
-      },
+    name: "Medine",
+    labels: " Din Records, Because Music, WMI, Stars de rue",
+    dateNaissance: "Date de Naissance : 1983",
+    picture: "https://upload.wikimedia.org/wikipedia/commons/c/c7/GRANDm%C3%A9dine.jpg",
+    genre: ["Hip-hop", "Rap Français", "Rap politique", "Rap Hardcore", "Rap conscient"],
+    album : ["11 Septembre, récit du 11e jour", "Jihad, le plus grand combat contre soi-même", "Arabian Panther", "Protest Song", "Storyteller", "Grand Medine"],
+    link : "https://www.youtube.com/watch?v=tHUp9Bb9_Sk"
+    },
+
