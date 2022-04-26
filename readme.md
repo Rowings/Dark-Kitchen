b02d90a6d01c7601995999359eacc50f57a44c1a
Rowings <zakariaazarkan@gmail.com>
2022-04-26 16:54:10 +0200

    3/4 des arrays

---
+++ readme.md
@@ -0,0 +1,19 @@
+Breakfast : 
+	Avocado Coffee 
+	Egg Pizza
+	Sausage Waffle
+	Choco Bacon
+
+Lunch : 
+	Burger Cookie
+	Choco Pasta
+	Weird Lunch
+	
+Dinner :
+Bacon Oreo
+Cream Chips
+Honey Pizza
+
+Drink :
+Banana Water 
+Pickle Granita
--- script.js
+++ script.js
@@ -1,52 +1,68 @@
-
-
-
-
-
-
-
-const collection = [
+const foodName = [
     {
-      type: "Tacos",
-      genre: " Simple, Double, Triple, Gratinée",
-      viande: "Nuggets, Tenders, Cordon Bleu, Steak, Veggie, Falafel",
-      sauce: "Andalouse, Brazil, Samouraï, Barbecue, Blanche, Curry-Ketchup, Biggie, Chili-mayo, Ketchup, Mayonnaise, Americain ",
-      price: 15,
-    },
+        foodName : "Avocado Coffee",
+        foodIMG : 'img/avocadoCoffee.jpg',
+        price : 250,
+        type : "Breakfast",
+        description : "La douceur de l'avocat accompagné de l'amertume du café vous offrira une expérience innoubliable de dégout"
 
-    {
-    type: "Tacos",
-    genre: "Double",
-    viande: "Nuggets, Tenders, Cordon Bleu, Steak, Veggie, Falafel",
-    sauce: "Andalouse, Brazil, Samouraï, Barbecue, Blanche, Curry-Ketchup, Biggie, Chili-mayo, Ketchup, Mayonnaise, Americain ",
-    price: 35,
     },
     {
-    type: "Tacos",
-    genre: " Triple",
-    viande: "Nuggets, Tenders, Cordon Bleu, Steak, Veggie, Falafel",
-    sauce: "Andalouse, Brazil, Samouraï, Barbecue, Blanche, Curry-Ketchup, Biggie, Chili-mayo, Ketchup, Mayonnaise, Americain ",
-    price : 45,
+        foodName : "Egg Pizza",
+        foodIMG : 'img/breakfast/eggPizza.jpg',
+        price : 150,
+        type : "Breakfast",
+        description : "Une envie de protéine ? Mais vous refusez d'abandonner votre plat préféré ? Pas de soucis ! Nous avons créer pour vous après plus de 5 ans de R&D la 'Egg Pizza' un massacre culinaire"
+
     },
     {
-    type: "Tacos",
-    genre: "Gratinée",
-    viande: "Nuggets, Tenders, Cordon Bleu, Steak, Veggie, Falafel",
-    sauce: "Andalouse, Brazil, Samouraï, Barbecue, Blanche, Curry-Ketchup, Biggie, Chili-mayo, Ketchup, Mayonnaise, Americain ",
-    price : 65,
-    },
-  
-  
-  
+        foodName : "Saussage Waffle",
+        foodIMG : 'img/breakfast/eggPizza.jpg',
+        price : 150,
+        type : "Breakfast",
+        description : "Une envie de protéine ? Mais vous refusez d'abandonner votre plat préféré ? Pas de soucis ! Nous avons créer pour vous après plus de 5 ans de R&D la 'Egg Pizza' un massacre culinaire"
 
-
+    }
     {
-    name: "Medine",
-    labels: " Din Records, Because Music, WMI, Stars de rue",
-    dateNaissance: "Date de Naissance : 1983",
-    picture: "https://upload.wikimedia.org/wikipedia/commons/c/c7/GRANDm%C3%A9dine.jpg",
-    genre: ["Hip-hop", "Rap Français", "Rap politique", "Rap Hardcore", "Rap conscient"],
-    album : ["11 Septembre, récit du 11e jour", "Jihad, le plus grand combat contre soi-même", "Arabian Panther", "Protest Song", "Storyteller", "Grand Medine"],
-    link : "https://www.youtube.com/watch?v=tHUp9Bb9_Sk"
-    },
+        foodName : "Choco Bacon",
+        foodIMG : 'img/breakfast/chocoBacon.jpg',
+        price : 150,
+        type : "Breakfast",
+        description : "Faut il vraiment une description pour ce plat tout droit descendu des cieux ? Non. Mange et ferme là, si tu vomis, ravale le."
 
+    },
+    {
+        foodName : "Burger Cookie",
+        foodIMG : 'img/lunch/burgerCookie.jpg',
+        price : 389,
+        type : "Lunch",
+        description : "Le moeulleux du cookie, la tendresse de la viande vous ferons retombé dans votre enfance, vous savez cette époque, ou vous mangiez tout ce que vous trouviez sans crainte DISCLAIMER : Nous ne sommes pas réponsable en cas de salmonelle "
+
+    },
+    {
+        foodName : "Choco Pasta",
+        foodIMG : 'img/lunch/chocoPasta.jpg',
+        price : 785,
+        type : "Lunch",
+        description : "Ce plat à une histoire phénomenale, connaissez vous Vin Diesel ? Eh bien il n'a absolument rien avoir dans cette histoire. c'est Volkswagn Van notre cuisinier en chef qui un jour à trébuché dans la cuisine, son coude à taper contre le pot de Nutello qui est tombé dans son assiette de pâtes au beurre, il demanda au stagiaire de gouté car il pensais que le pot était périmé, ce qui était effectivement le cas après vérification, mais cela donna au pâtes un goût unique (plutot un gout de moissi en vrai)"
+
+    },
+    {
+        foodName : "Weird Lunch",
+        foodIMG : 'img/lunch/worstLunch.jpg',
+        price : 895,
+        type : "Lunch",
+        description : "Vous êtes une personne qui met un short en hiver ? Le genre de personne qui commande son BigMac sans viande ? Vous aimez l'ananas sur les pizza ? Vous gardez vos chaussettes pendant l'amour ? Ce plat est fait pour vous."
+    },
+    {
+        foodName : "Bacon Oreo",
+        foodIMG : 'img/dinner/baconOreo.jpg',
+        price : 389,
+        type : "Dinner",
+        description : "Le meilleurs des deux mondes reunis pour votre plus grand plaisir" 
+    },
+    {
+
+    }
+
+]
