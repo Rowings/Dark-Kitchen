a94a16ce5a44f8b2eeffd33176b173033d10a90d
steffanie <steffanieahrensedu@gmail.com>
2022-04-27 09:39:18 +0200

    one

---
+++ script.js
@@ -0,0 +1,11 @@
+const food = [
+    {
+     Breakfast : "Avocado Coffee, Egg Pizza, Sausage Waffle, Choco Bacon",
+
+    Lunch : "Burger Cookie, Choco Pasta, Weird Lunch",
+
+    Dinner : "Bacon Oreo, Cream Chips, Honey Pizza",
+
+    Drink : "Banana Water, Pickle Granita",
+    }
+]
